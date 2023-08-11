18c30d3cf80fa47fafcb67a1a4f1108c41d44122
ubuntu6ty9 <samted.uche@gmail.com>
2023-08-11 14:11:17 +0100

    updated init

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgresql+psycopg2://teddyCodex:32JZTUHjXPRB@ep-muddy-meadow-66562690.us-east-2.aws.neon.tech/neondb"
+] = "postgresql://guest_manager_db_user:SNOPJ7lb5WwXbnUuNXk9NvrFeLO8XbKk@dpg-cjb29rrbq8nc73b1iopg-a.oregon-postgres.render.com/guest_manager_db"
 db = SQLAlchemy(app)
 
 from app import routes
@@ -13,3 +13,5 @@ from app import routes
 # "postgresql+psycopg2://teddyCodex:32JZTUHjXPRB@ep-muddy-meadow-66562690.us-east-2.aws.neon.tech/neondb"
 
 # "postgresql://postgres:007700@localhost/guest_list"
+
+# postgresql://guest_manager_db_user:SNOPJ7lb5WwXbnUuNXk9NvrFeLO8XbKk@dpg-cjb29rrbq8nc73b1iopg-a.oregon-postgres.render.com/guest_manager_db
