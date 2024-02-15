894d0afee43b57de0bd9e395c155dc9e53590190
ubuntu6ty9 <samted.uche@gmail.com>
2024-02-15 07:04:32 +0100

    update requirements and init file with supabase uri

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgres://postgres.lpvniqvdyqnbtadkijyf:!Tunerwell007@aws-0-us-east-1.pooler.supabase.com:5432/postgres"
+] = "postgresql://postgres.lpvniqvdyqnbtadkijyf:!Tunerwell007@aws-0-us-east-1.pooler.supabase.com:5432/postgres"
 db = SQLAlchemy(app)
 
 from app import routes
@@ -15,3 +15,6 @@ from app import routes
 # "postgresql://postgres:007700@localhost/guest_list"
 
 # postgresql://guest_manager_db_user:SNOPJ7lb5WwXbnUuNXk9NvrFeLO8XbKk@dpg-cjb29rrbq8nc73b1iopg-a.oregon-postgres.render.com/guest_manager_db
+
+
+#  "postgres://postgres.lpvniqvdyqnbtadkijyf:!Tunerwell007@aws-0-us-east-1.pooler.supabase.com:5432/postgres"
